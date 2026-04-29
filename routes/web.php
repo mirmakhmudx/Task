a8a3dbcc1636476c95835b0c9e16fb64c92a0b7a
Mirmahmud03 <akeforex55@gmail.com>
2026-04-29 12:48:28 +0500

    add docker and login removed

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-    Route::get('/cabinet', [\App\Http\Controllers\Cabinet\HomeController::class, 'index'] )->name('cabinet');
 });
 
 require __DIR__.'/auth.php';
