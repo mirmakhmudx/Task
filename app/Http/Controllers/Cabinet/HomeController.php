70b8a0b7fff35e7c6346ed82db776e022ab65b94
Mirmahmud03 <akeforex55@gmail.com>
2026-08-19 14:25:04 +0500

    update

--- app/Http/Controllers/Cabinet/HomeController.php
+++ app/Http/Controllers/Cabinet/HomeController.php
@@ -10,8 +10,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
-{
-    public function index()
+{public function index()
     {
         $userId = Auth::id();
 
